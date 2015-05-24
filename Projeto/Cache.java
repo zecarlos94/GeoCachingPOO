3c88640f7aaa406ca40ccf706f6a5b1e30c5cfe0
Gstavo <gustavocg@live.com.pt>
2015-05-24 15:07:20 +0100

    .

--- Projeto/Cache.java
+++ Projeto/Cache.java
@@ -71,10 +71,7 @@ public abstract class Cache
      */
     public abstract int getGeoCoins();
     
-     /**
-     *  Devolve os geoCoins obtidos pelo utilizador na cache
-     */
-    public abstract int getGeoCoinsTotais();
+
     
     /**
      * Setters
