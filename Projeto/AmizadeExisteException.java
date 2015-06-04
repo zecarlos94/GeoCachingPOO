5b96a972f8ebe38dbd85a08934eb2c16f5c79837
Gstavo <gustavocg@live.com.pt>
2015-06-04 14:42:48 +0100

    ,

---
+++ Projeto/AmizadeExisteException.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class AmizadeExisteException here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AmizadeExisteException extends Exception
+{
+ 
+    public AmizadeExisteException()
+    {
+       super();
+    }
+
+}
---
+++ Projeto/Tesouro.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface Tesouro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Tesouro
+{
+     /**
+     *  Devolve os geoCoins base das Caches
+     */
+    public abstract int getGeoCoins();
+}
